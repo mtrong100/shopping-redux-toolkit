595c0773b3d3847c81653cb07ce3f1070b52d988
trongsieucapvippro <94774477+trongsieucapvippro@users.noreply.github.com>
2023-06-08 10:11:58 +0700

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux-toolkit/store";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter basename="/shopping-redux-toolkit">
+  <HashRouter>
     <Provider store={store}>
       <App />
       <ToastContainer />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
